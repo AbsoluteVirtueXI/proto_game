08f3a4d26c5c27501e93ee62db67bbc05d85719f
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-05-16 03:29:41 +0200

    tile is  32.0

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,6 @@ pub use player::PlayerPlugin;
 pub use tilemap::TileMapPlugin;
 
 /// Constants
-pub const TILE_SIZE: f32 = 0.1;
+pub const TILE_SIZE: f32 = 32.0;
 pub const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
